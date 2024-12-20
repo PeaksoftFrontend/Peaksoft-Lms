8fced1c306f22569056f63ad4388039bb47a9ee0
gulnur05kj <abdrasitovagulnur830@gmail.com>
2024-12-20 13:59:21 +0300

    fexed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { TableTaxt } from "./components/UI/TableTaxt";
-import { Columns, Data } from "./utils/constants/table";
-
 export const App = () => {
   // console.log("render");
 
-  return (
-    <div>
-      <div>
-        <TableTaxt rows={Data} columns={Columns}/>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
