1d7bd17d4c5e9ceb724eef7d6c8ad6b49cb840ee
Alfia <alfia.29.2004@icloud.com>
2024-12-21 14:33:39 +0300

    finished radio checkbox

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,5 +1,4 @@
-import { styled } from "@mui/material";
-import { Checkbox as MuiCheckbox } from "@mui/material";
+import { Checkbox as MuiCheckbox, FormControlLabel } from "@mui/material";
 
 export const Checkbox = ({
   checked,
@@ -10,29 +9,30 @@ export const Checkbox = ({
   ...props
 }) => {
   return (
-    <label>
-      <StyledCheckbox
-        checked={checked}
-        onChange={onChange}
-        disabled={disabled}
-        {...props}
-      />
-      {label}
-    </label>
+    <FormControlLabel
+      checked={checked}
+      onChange={onChange}
+      disabled={disabled}
+      label={label}
+      {...props}
+      control={
+        <MuiCheckbox
+          sx={{
+            color: "#000000",
+            transition: "color 0.3s",
+
+            "&:hover": {
+              color: "#3772FF",
+            },
+            "&.Mui-checked": {
+              color: "#3772FF",
+            },
+            "&.Mui-disabled": {
+              color: "#1C1B1F1F",
+            },
+          }}
+        />
+      }
+    />
   );
 };
-
-const StyledCheckbox = styled(MuiCheckbox)(() => ({
-  color: "#000000",
-  transition: "color 0.3s",
-
-  "&:hover": {
-    color: "#3772FF",
-  },
-  "&.Mui-checked": {
-    color: "#3772FF",
-  },
-  "&.Mui-disabled": {
-    color: "#1C1B1F1F",
-  },
-}));
