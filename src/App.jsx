d7310daebe859ae24dc562a7f72237c7bbc354cf
ibadat <abdurasu10vaibd@gmail.com>
2024-12-24 01:04:33 +0300

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import { ModalComponent } from "./components/UI/ModalComponent";
-
 export const App = () => {
-  return (
-    <div>
-      <ModalComponent height="581px" />
-    </div>
-  );
+  return <div></div>;
 };
