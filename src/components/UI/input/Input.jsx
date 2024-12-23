9046b5b7e8cccbb573b414694f005fc1adad8197
ibadat <abdurasu10vaibd@gmail.com>
2024-12-23 23:53:27 +0300

    input complete

--- src/components/UI/input/Input.jsx
+++ src/components/UI/input/Input.jsx
@@ -1,4 +1,6 @@
-import { TextField } from "@mui/material";
+import { useState } from "react";
+import { styled, TextField, InputAdornment, IconButton } from "@mui/material";
+import { Icons } from "../../../assets";
 
 const Input = ({
   label,
@@ -7,24 +9,57 @@ const Input = ({
   onChange,
   name,
   error = false,
-  helperText = "",
   fullWidth = true,
   ...props
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
-    <TextField
+    <StyledInput
       label={label}
-      type={type}
+      type={type === "password" && showPassword ? "text" : type}
       value={value}
       onChange={onChange}
       name={name}
       error={error}
-      helperText={helperText}
       fullWidth={fullWidth}
       variant="outlined"
+      InputProps={{
+        endAdornment: type === "password" && (
+          <InputAdornment position="end">
+            <IconButton
+              onClick={togglePasswordVisibility}
+              edge="end"
+              style={{
+                cursor: "pointer",
+              }}
+            >
+              {showPassword ? <Icons.EyesGod /> : <Icons.Eyes />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
       {...props}
     />
   );
 };
 
 export default Input;
+
+const StyledInput = styled(TextField)(({ error }) => ({
+  "& .MuiOutlinedInput-root": {
+    borderRadius: "10px",
+    height: "42px",
+    borderColor: error ? "red" : "inherit",
+  },
+  "& .MuiOutlinedInput-notchedOutline": {
+    borderColor: error ? "red" : "inherit",
+  },
+  "&:hover .MuiOutlinedInput-notchedOutline": {
+    borderColor: error ? "red" : "#1976d2",
+  },
+}));
