5ad0e032f4577416791f053c453c8eddd984c993
gulnur05kj <abdrasitovagulnur830@gmail.com>
2025-01-09 19:26:42 +0300

    login

--- src/components/auth/FotgotPassword..jsx
+++ src/components/auth/FotgotPassword..jsx
@@ -1,14 +1,14 @@
-// import { styled } from "@mui/material";
-// import { Button } from "../UI/Button";
-// import Input from "../UI/input/Input";
+import { styled } from "@mui/material";
+import { Button } from "../UI/Button";
+import Input from "../UI/input/Input";
 
 export const FotgotPassword = () => {
   return (
     <div>
-      {/* <Modal> */}
-      {/* <h1>sdfghjk</h1> */}
-      {/* </Modal> */}
-      {/* <StyledBox>
+      {/* <Modal>
+        <h1>sdfghjk</h1>
+      </Modal> */}
+      <StyledBox>
         <StyledContainer>
           <StyledDiv>
             <StyledP>Забыли пароль?</StyledP>
@@ -29,44 +29,44 @@ export const FotgotPassword = () => {
             </div>
           </StyledWrapper>
         </StyledContainer>
-      </StyledBox> */}
+      </StyledBox>
     </div>
   );
 };
-// const StyledBox = styled("div")({
-//   alignItems: "center",
-//   justifyContent: "center",
-// });
-// const StyledText = styled("div")({
-//   padding: "16px 0 0 25px",
-// });
-// const StyledWrapper = styled("div")({
-//   display: "flex",
-//   alignItems: "center",
-//   flexDirection: "column",
-//   gap: "20px",
-// });
-// const StyledP = styled("p")({
-//   fontWeight: "400",
-//   fontSize: "20px",
-//   lineheight: "27px",
-//   color: "rgba(255,255,255,1)",
-//   padding: "25px 0 0 190px",
-// });
-// const StyledDiv = styled("div")({
-//   height: "68px",
-//   background: "rgba(31,110,212,1)",
-//   borderRadius: "5px",
-// });
-// const StyledButton = styled(Button)({
-//   width: "483px",
-//   borderRadius: "8px",
-// });
-// const StyledInput = styled(Input)({
-//   width: "491px",
-// });
-// const StyledContainer = styled("div")({
-//   // width: "545px",
-//   // height: "241px",
-//   borderRadius: "10px",
-// });
+const StyledBox = styled("div")({
+  alignItems: "center",
+  justifyContent: "center",
+});
+const StyledText = styled("div")({
+  padding: "16px 0 0 25px",
+});
+const StyledWrapper = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  gap: "20px",
+});
+const StyledP = styled("p")({
+  fontWeight: "400",
+  fontSize: "20px",
+  lineheight: "27px",
+  color: "rgba(255,255,255,1)",
+  padding: "25px 0 0 190px",
+});
+const StyledDiv = styled("div")({
+  height: "68px",
+  background: "rgba(31,110,212,1)",
+  borderRadius: "5px",
+});
+const StyledButton = styled(Button)({
+  width: "483px",
+  borderRadius: "8px",
+});
+const StyledInput = styled(Input)({
+  width: "491px",
+});
+const StyledContainer = styled("div")({
+  // width: "545px",
+  // height: "241px",
+  borderRadius: "10px",
+});
